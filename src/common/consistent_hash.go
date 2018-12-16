a3d4c06e7d31c3899929854d33af2d774a586eab
kejie <kejie1235@163.com>
2018-12-16 19:12:48 +0800

    一致性hash

--- src/common/consistent_hash.go
+++ src/common/consistent_hash.go
@@ -1,5 +1,12 @@
 package common
 
+import (
+	"strconv"
+	"hash/crc64"
+	"sort"
+	"fmt"
+)
+
 /*
 	implement consistent_hash by golang
 
@@ -13,33 +20,74 @@ type ServerNode struct {
 	ServerName		string
 }
 
-
-type ConsistentHash struct {
-	VirtualNodeNumber	int
-	HashCircle 			map[int64]*ServerNode
-	HashFunc			func(key string) int64
+type HashCircleItem struct{
+	Key  uint64
+	Node ServerNode
 }
 
-func NewConsistentHash(virtualNodeNumber int) {
-	this := new(ConsistentHash)
+type HashCircleCmpWrapper struct {
+	HashCircle []HashCircleItem
+	Cmp func(p, q *HashCircleItem) bool
+}
+
+func (wrapper HashCircleCmpWrapper) Len() int {
+	return len(wrapper.HashCircle)
+}
+
+func (wrapper HashCircleCmpWrapper) Swap(i ,j int) {
+	wrapper.HashCircle[i], wrapper.HashCircle[j] = wrapper.HashCircle[j], wrapper.HashCircle[i]
+}
+
+func (wrapper HashCircleCmpWrapper) Less(i,j int) bool{
+	return wrapper.Cmp(&wrapper.HashCircle[i], &wrapper.HashCircle[j])
+}
+
+type ConsistentHashImp struct {
+	VirtualNodeNumber int
+	Wrapper           HashCircleCmpWrapper
+}
+
+func NewConsistentHashImp(virtualNodeNumber int, cmp func(p, q *HashCircleItem) bool) *ConsistentHashImp{
+	this := new(ConsistentHashImp)
 	this.VirtualNodeNumber = virtualNodeNumber
-	this.HashCircle = make(map[int64] *ServerNode)
-	//this.HashFunc = hash.
+	this.Wrapper = HashCircleCmpWrapper{}
+	this.Wrapper.Cmp = cmp
+	return this
 }
 
-func NewConsistentHashByHash(virtualNodeNumber int, ob func(key string) int64, ){
-	this := new(ConsistentHash)
-	this.VirtualNodeNumber = virtualNodeNumber
-	this.HashFunc = ob
+func (this *ConsistentHashImp)sortHashCircleByCryptoKey() {
+	sort.Sort(this.Wrapper)
 }
 
-func Add(serverList []ServerNode) {
+func (this *ConsistentHashImp)Add(serverList []ServerNode) {
+	for _, server := range serverList {
+		for repIndex := 1; repIndex <= this.VirtualNodeNumber; repIndex++ {
+			crypto := server.ServerIP + "#" + strconv.Itoa(repIndex)
+			key := crc64.Checksum([]byte(crypto), crc64.MakeTable(crc64.ECMA))
 
+			fmt.Printf("虚拟节点[%s]被添加, hash值为%d\n", crypto, key)
+			item := HashCircleItem{
+				key,
+				server,
+			}
+			this.Wrapper.HashCircle = append(this.Wrapper.HashCircle, item)
+		}
+	}
+
+	this.sortHashCircleByCryptoKey()
+	//fmt.Print(sort.IsSorted(this.Wrapper))
+	//fmt.Print(this.Wrapper.HashCircle)
 }
 
 
-func Get(key string) {
+func (this *ConsistentHashImp)Get(key string) (*ServerNode) {
+	cryptoKey := crc64.Checksum([]byte(key), crc64.MakeTable(crc64.ECMA))
 
+	for _, item := range this.Wrapper.HashCircle {
+		if item.Key > cryptoKey {
+			return &item.Node
+		}
+	}
+	return nil
 }
-
 
--- src/driver/driver.go
+++ src/driver/driver.go
@@ -1,13 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"crypto/md5"
+	"hash/crc64"
+	"common"
+	"strconv"
+)
 
 func main()  {
 	fmt.Printf("hello world!\n")
 
-	test_consistent_hash()
+	testConsistentHash()
 }
 
-func test_consistent_hash()  {
+func testMd5Func() {
+	testString := "func1235"
+	Md5Inst := md5.New()
+	Md5Inst.Write([]byte(testString))
+	result := Md5Inst.Sum([]byte(""))
+	fmt.Printf("%d\n\n", result)
+}
+
+func testCrc32Func() {
+	str := "123456asdfgg"
+
+	result := crc64.Checksum([]byte(str), crc64.MakeTable(crc64.ISO))
+	fmt.Printf("%d\n", result)
+}
+
+func testConsistentHash()  {
 	
+	impl := common.NewConsistentHashImp(20, func(p, q *common.HashCircleItem) bool {
+		return p.Key < q.Key
+	})
+
+	serverList := []common.ServerNode{
+		{"192.168.0.1", "Svr01"},
+		{"192.168.0.2", "Svr02"},
+		{"192.168.0.3","Svr03"},
+		{"192.168.0.4", "Svr04"},
+		{"192.168.0.5", "Svr05"},
+		{"192.168.0.6", "Svr06"},
+	}
+
+	impl.Add(serverList)
+
+	server := "127.0.0."
+
+	statics := make(map[string]int)
+	for index:=1; index<100; index++ {
+
+		str := server + strconv.Itoa(index)
+		node := impl.Get(str)
+
+		if node != nil {
+			fmt.Printf("[%s]节点的hash值为 %d, 被路由到的节点为 [%s]\n",
+							str, crc64.Checksum([]byte(str), crc64.MakeTable(crc64.ECMA)), node.ServerIP)
+
+			statics[node.ServerIP]++
+		}
+	}
+	fmt.Printf("一致性Hash命中情况: %v", statics)
 }
